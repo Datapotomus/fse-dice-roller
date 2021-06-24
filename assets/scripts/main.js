dc3233896eba2d63b4a3b8c18c71c1315b567af9
MATTHEW THERAULT <MATTHEW.THERAULT@LibertyMutual.com>
2021-06-24 14:51:28 -0400

    worked on it during working session.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,8 +2,10 @@ let dieRolls = [];
 
 let rollDice = document.querySelector("#rollBtn");
 let numDice = document.querySelector("#diceNum");
+let totalDiceRolls = document.querySelector("#diceTotal");
 let showRolls = document.querySelector("#allRollsBtn")
 let diceList = document.querySelector("#rollsList");
+
 
 function diceImg(num){
   let imgElement = document.createElement('img')
@@ -16,9 +18,9 @@ function diceImg(num){
 
 
 // Pulled from mdn docs
-function getDiceResult() {
-  min = Math.ceil(1);
-  max = Math.floor(6);
+function getDiceResult(min,max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
@@ -29,11 +31,11 @@ function arraySum(total, num) {
 rollDice.addEventListener("click", function () {
   let numberOfDice = numDice.value;
   for (let index = 0; index < numberOfDice; index++) {
-    dieRolls.push(getDiceResult());
+    dieRolls.push(getDiceResult(1,6));
   }
 
   let diceTotal = dieRolls.reduce(arraySum);
-  document.querySelector("#diceTotal").innerHTML = diceTotal
+  totalDiceRolls.innerHTML = diceTotal
   console.log(numberOfDice)
   console.log(`DieRolls ${dieRolls}`)
   console.log(`DiceTotal ${diceTotal}`)
@@ -51,7 +53,15 @@ showRolls.addEventListener("click", function () {
   console.log(diceList)
 });
 
+// showRolls.addEventListener("click", function () {
+//   document.querySelectorAll("li").forEach(e => e.parentNode.removeChild(e));
+//   for (let index = 0; index < dieRolls.length; index++) {
+//     let numberRolled = dieRolls[index];
+//     let diceImage = `<img src="./assets/images/Dice-${dieRolls[index]}-b.svg.png" alt="picture of a dice" width="30"></img>`
+//     const newDiceString='<li class="dice">'+numberRolled+ " = " + diceImage + "</li>";
+//     diceList.innerHTML+= newDiceString
+//   }
+//   console.log(diceList)
+// });
 
-
-
-console.log(document.querySelector("#diceTotal").innerHTML)
+console.log(totalDiceRolls.innerHTML)
